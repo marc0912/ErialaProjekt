45371e7d8c4f91357324a2eea1206539f0f80941
PeterkalM <peterkallaste@hot.ee>
2018-11-29 21:58:00 +0200

    Fix mail

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -9,18 +9,17 @@ if(empty($_POST['name'])      ||
    echo "No arguments Provided!";
    return false;
    }
-   
+
 $name = strip_tags(htmlspecialchars($_POST['name']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
-   
-// Create the email and send the message
-$to = 'yourname@yourdomain.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";   
+
+$to = 'saamisemail@domeen.eest';
+$email_subject = "Veebilehe kontakt:  $name";
+$email_body = "See sõnum tuli veebilehelt.\n\n"."Kontakteeruja andmed:\nNimi: $name\nEmail: $email_address\nTelefon: $phone\n\nSõnum:\n$message";
+$headers = "From: noreply@domeen.ee\n";
+$headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body,$headers);
-return true;         
+return true;
 ?>
